dbfbe4294bfe972ea93d41596ba47d744a7e0b05
han0224 <dbn0224@gmail.com>
2023-07-13 11:08:55 +0900

    feat: estimation select css

--- client/src/views/EstimationSelectView.vue
+++ client/src/views/EstimationSelectView.vue
@@ -1,35 +1,35 @@
 <template>
-  <div>
+  <div class="estimation_select">
     <h1>estimation select</h1>
-    <div>
-      class select
-      <select name="class" id="class" v-model="selectedClass">
-        <option value="none" disabled selected>다음 중 하나를 선택해주세요</option>
-        <option v-for="(type, index) in classTypes" :key="index" :value="type">
-          {{ type }}
-        </option>
-      </select>
+    <div class="select_wrap_wrap">
+      <div class="select_wrap">
+        <h2>class select</h2>
+        <select name="class" id="class" v-model="selectedClass">
+          <option value="none" disabled selected>다음 중 하나를 선택해주세요</option>
+          <option v-for="(type, index) in classTypes" :key="index" :value="type">
+            {{ type }}
+          </option>
+        </select>
+      </div>
+      <div class="select_wrap">
+        <h2>seminar type select</h2>
+        <select name="class" id="type" v-model="selectedType">
+          <option value="none" disabled selected>다음 중 하나를 선택해주세요</option>
+          <option v-for="(type, index) in seminarTypes[selectedClass]" :key="index" :value="type">
+            {{ type }}
+          </option>
+        </select>
+      </div>
     </div>
-    <div>
-      seminar type select
-      <select name="class" id="class" v-model="selectedType">
-        <option value="none" disabled selected>다음 중 하나를 선택해주세요</option>
-        <option v-for="(type, index) in seminarTypes[selectedClass]" :key="index" :value="type">
-          {{ type }}
-        </option>
-      </select>
-    </div>
-    <div>
-      <button
-        :disabled="selectedClass === 'none' || selectedType === 'none'"
-        @click="
-          () =>
-            goPage({ name: `estimation list`, state: { clazz: selectedClass, type: selectedType } })
-        "
-      >
-        평가하러 가기
-      </button>
-    </div>
+    <button
+      :disabled="selectedClass === 'none' || selectedType === 'none'"
+      @click="
+        () =>
+          goPage({ name: `estimation list`, state: { clazz: selectedClass, type: selectedType } })
+      "
+    >
+      평가하러 가기
+    </button>
   </div>
 </template>
 
@@ -39,7 +39,6 @@ import { goPage } from '../utils/pages.js';
 
 const classTypes = ref(['service', 'engineering', 'ai']);
 const seminarTypes = ref({
-  none: ['다음 중 하나를 선택해주세요'],
   service: ['front', 'back', 'cloud'],
   engineering: ['1', '2', '3'],
   ai: ['1', '2', '3']
@@ -48,4 +47,57 @@ const selectedClass = ref('none');
 const selectedType = ref('none');
 </script>
 
-<style lang="scss" scoped></style>
+<style scoped>
+.estimation_select {
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 16px;
+}
+.estimation_select > h1 {
+  font-size: 48px;
+  font-weight: 700;
+}
+
+.select_wrap_wrap {
+  display: flex;
+  gap: 64px;
+  flex-direction: row;
+}
+.select_wrap {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.select_wrap > h2 {
+  font-size: 32px;
+}
+.select_wrap > select {
+  height: 32px;
+}
+
+button {
+  border: 1px solid black;
+  padding: 10px 20px;
+  border-radius: 30px;
+  background: blue;
+  color: white;
+  font-size: 24px;
+  cursor: pointer;
+  margin: 20px;
+}
+
+button:disabled {
+  border: 1px solid black;
+  padding: 10px 20px;
+  border-radius: 30px;
+  background: rgb(133, 133, 152);
+  color: white;
+  font-size: 24px;
+  margin: 20px;
+  cursor: not-allowed;
+}
+</style>
