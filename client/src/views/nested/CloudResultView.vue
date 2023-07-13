532a94549e5cf3ce6ea61485832c2c54feccb299
Cheol-Cheol <dream174822@gmail.com>
2023-07-13 12:27:41 +0900

    feat: 더미 데이터로 ResultView 구현

--- client/src/views/nested/CloudResultView.vue
+++ client/src/views/nested/CloudResultView.vue
@@ -1,63 +1,224 @@
 <template>
   <div>
-    <h1>클라우드 세미나 결과화면</h1>
-    <h2>top 3</h2>
-    <div>
-      <div>card1</div>
-      <div>card2</div>
-      <div>card3</div>
-    </div>
-    <div>
-      <h2>all teams</h2>
-      <div v-for="(team, idx) in teams" :key="idx">
-        {{ team.teamNo }}
-      </div>
-    </div>
+    <h1 style="font-size: 20px font-weight: bold;"># 클라우드 세미나 결과화면</h1>
+    <PrintResultView :top3="top3" :allTeams="allTeams" />
   </div>
 </template>
 
 <script setup>
 import { ref } from 'vue';
+import PrintResultView from '../../components/result/PrintResultView.vue';
 
-const DUMMY = [
+const TOP3 = [
   {
-    teamNo: '1팀',
-    subject: 'SOLID #1',
-    member: ['철원', '윤서']
+    clazzName: '클라우드 서비스 개발',
+    seminarTypeName: 'Web 3.0',
+    subjectId: 20,
+    subjectTitle: '백엔드 세미나',
+    subjectOrder: 11,
+    memberList: [
+      {
+        id: 9,
+        name: '이은엽'
+      },
+      {
+        id: 20,
+        name: '김승연'
+      }
+    ]
   },
   {
-    teamNo: '2팀',
-    subject: 'SOLID #2',
-    member: ['철원', '윤서']
+    clazzName: '클라우드 서비스 개발',
+    seminarTypeName: '인증과 인가',
+    subjectId: 21,
+    subjectTitle: '백엔드 세미나',
+    subjectOrder: 12,
+    memberList: [
+      {
+        id: 11,
+        name: '박진영'
+      },
+      {
+        id: 18,
+        name: '김용민'
+      }
+    ]
   },
   {
-    teamNo: '3팀',
-    subject: 'SOLID #3',
-    member: ['철원', '윤서']
-  },
-  {
-    teamNo: '4팀',
-    subject: 'SOLID #4',
-    member: ['철원', '윤서']
-  },
-  {
-    teamNo: '5팀',
-    subject: 'SOLID #5',
-    member: ['철원', '윤서']
-  },
-  {
-    teamNo: '6팀',
-    subject: 'SOLID #6',
-    member: ['철원', '윤서']
-  },
-  {
-    teamNo: '7팀',
-    subject: 'SOLID #7',
-    member: ['철원', '윤서']
+    clazzName: '클라우드 서비스 개발',
+    seminarTypeName: 'SOLID',
+    subjectId: 19,
+    subjectTitle: '백엔드 세미나',
+    subjectOrder: 10,
+    memberList: [
+      {
+        id: 3,
+        name: '황철원'
+      },
+      {
+        id: 4,
+        name: '한윤서'
+      }
+    ]
   }
 ];
 
-const teams = ref(DUMMY);
-</script>
+const DUMMY = [
+  {
+    clazzName: '클라우드 서비스 개발',
+    seminarTypeName: 'Web 3.0',
+    subjectId: 20,
+    subjectTitle: '백엔드 세미나',
+    subjectOrder: 11,
+    memberList: [
+      {
+        id: 9,
+        name: '이은엽'
+      },
+      {
+        id: 20,
+        name: '김승연'
+      }
+    ]
+  },
+  {
+    clazzName: '클라우드 서비스 개발',
+    seminarTypeName: '인증과 인가',
+    subjectId: 21,
+    subjectTitle: '백엔드 세미나',
+    subjectOrder: 12,
+    memberList: [
+      {
+        id: 11,
+        name: '박진영'
+      },
+      {
+        id: 18,
+        name: '김용민'
+      }
+    ]
+  },
+  {
+    clazzName: '클라우드 서비스 개발',
+    seminarTypeName: 'SOLID',
+    subjectId: 19,
+    subjectTitle: '백엔드 세미나',
+    subjectOrder: 10,
+    memberList: [
+      {
+        id: 3,
+        name: '황철원'
+      },
+      {
+        id: 4,
+        name: '한윤서'
+      }
+    ]
+  },
+  {
+    clazzName: '클라우드 서비스 개발',
+    seminarTypeName: 'Web 3.0',
+    subjectId: 20,
+    subjectTitle: '백엔드 세미나',
+    subjectOrder: 11,
+    memberList: [
+      {
+        id: 9,
+        name: '이은엽'
+      },
+      {
+        id: 20,
+        name: '김승연'
+      }
+    ]
+  },
+  {
+    clazzName: '클라우드 서비스 개발',
+    seminarTypeName: '인증과 인가',
+    subjectId: 21,
+    subjectTitle: '백엔드 세미나',
+    subjectOrder: 12,
+    memberList: [
+      {
+        id: 11,
+        name: '박진영'
+      },
+      {
+        id: 18,
+        name: '김용민'
+      }
+    ]
+  },
+  {
+    clazzName: '클라우드 서비스 개발',
+    seminarTypeName: 'SOLID',
+    subjectId: 19,
+    subjectTitle: '백엔드 세미나',
+    subjectOrder: 10,
+    memberList: [
+      {
+        id: 3,
+        name: '황철원'
+      },
+      {
+        id: 4,
+        name: '한윤서'
+      }
+    ]
+  },
+  {
+    clazzName: '클라우드 서비스 개발',
+    seminarTypeName: 'Web 3.0',
+    subjectId: 20,
+    subjectTitle: '백엔드 세미나',
+    subjectOrder: 11,
+    memberList: [
+      {
+        id: 9,
+        name: '이은엽'
+      },
+      {
+        id: 20,
+        name: '김승연'
+      }
+    ]
+  },
+  {
+    clazzName: '클라우드 서비스 개발',
+    seminarTypeName: '인증과 인가',
+    subjectId: 21,
+    subjectTitle: '백엔드 세미나',
+    subjectOrder: 12,
+    memberList: [
+      {
+        id: 11,
+        name: '박진영'
+      },
+      {
+        id: 18,
+        name: '김용민'
+      }
+    ]
+  },
+  {
+    clazzName: '클라우드 서비스 개발',
+    seminarTypeName: 'SOLID',
+    subjectId: 19,
+    subjectTitle: '백엔드 세미나',
+    subjectOrder: 10,
+    memberList: [
+      {
+        id: 3,
+        name: '황철원'
+      },
+      {
+        id: 4,
+        name: '한윤서'
+      }
+    ]
+  }
+];
 
-<style scoped></style>
+const allTeams = ref(DUMMY);
+const top3 = ref(TOP3);
+</script>
