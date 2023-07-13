93fd6bdcb9bd3a81e11b4bbac3cbd769c17bdced
Cheol-Cheol <dream174822@gmail.com>
2023-07-13 10:54:46 +0900

    feat: 중첩 라우터 페이지 세팅

--- client/src/views/ResultView.vue
+++ client/src/views/ResultView.vue
@@ -1,23 +1,47 @@
 <template>
   <div>
-    <h1>Result View</h1>
-    <div>
-      <h2>top 3</h2>
-      <div>
-        <div>card1</div>
-        <div>card2</div>
-        <div>card3</div>
+    <h1>클라우드 서비스 개발 세미나 결과</h1>
+    <div class="container">
+      <div class="nav">
+        <RouterLink to="/result/fe" active-class="active" style="text-decoration: none">
+          <div class="nav-item">프론트엔드</div>
+        </RouterLink>
+        <RouterLink to="/result/be" active-class="active" style="text-decoration: none">
+          <div class="nav-item">백엔드</div>
+        </RouterLink>
+        <RouterLink to="/result/cloud" active-class="active" style="text-decoration: none">
+          <div class="nav-item">클라우드</div>
+        </RouterLink>
       </div>
-    </div>
-    <div>
-      <h2>all teams</h2>
-      <div>
-        <div>cards</div>
-      </div>
+      <RouterView></RouterView>
     </div>
   </div>
 </template>
 
 <script setup></script>
 
-<style lang="scss" scoped></style>
+<style scoped>
+.container {
+  width: 1200px;
+  height: 100vh;
+  margin: 0 auto;
+}
+.nav {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 1200px;
+  background-color: #eee;
+}
+
+.nav-item {
+  color: black;
+  font-size: 20px;
+  width: 400px;
+  text-align: center;
+}
+
+.active {
+  background-color: rgb(71, 71, 252);
+}
+</style>
---
+++ client/src/views/nested/BeResultView.vue
@@ -0,0 +1,63 @@
+<template>
+  <div>
+    <h1 class="">클라우드 세미나 결과화면</h1>
+    <h2>top 3</h2>
+    <div>
+      <div>card1</div>
+      <div>card2</div>
+      <div>card3</div>
+    </div>
+    <div>
+      <h2>all teams</h2>
+      <div v-for="(team, idx) in teams" :key="idx">
+        {{ team.teamNo }}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+const DUMMY = [
+  {
+    teamNo: '1팀',
+    subject: 'SOLID #1',
+    member: ['철원', '윤서']
+  },
+  {
+    teamNo: '2팀',
+    subject: 'SOLID #2',
+    member: ['철원', '윤서']
+  },
+  {
+    teamNo: '3팀',
+    subject: 'SOLID #3',
+    member: ['철원', '윤서']
+  },
+  {
+    teamNo: '4팀',
+    subject: 'SOLID #4',
+    member: ['철원', '윤서']
+  },
+  {
+    teamNo: '5팀',
+    subject: 'SOLID #5',
+    member: ['철원', '윤서']
+  },
+  {
+    teamNo: '6팀',
+    subject: 'SOLID #6',
+    member: ['철원', '윤서']
+  },
+  {
+    teamNo: '7팀',
+    subject: 'SOLID #7',
+    member: ['철원', '윤서']
+  }
+];
+
+const teams = ref(DUMMY);
+</script>
+
+<style scoped></style>
