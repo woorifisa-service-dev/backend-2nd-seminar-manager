d7b2e5a1c8e7cd228c828e21a35113c73ba7bf8d
Dongyeol <eastheat10@gmail.com>
2023-07-19 15:18:54 +0900

    deploy

--- server/src/main/java/com/woorifisa/seminar/filter/CorsFilter.java
+++ server/src/main/java/com/woorifisa/seminar/filter/CorsFilter.java
@@ -1,10 +1,5 @@
 package com.woorifisa.seminar.filter;
 
-import static org.springframework.http.HttpMethod.DELETE;
-import static org.springframework.http.HttpMethod.GET;
-import static org.springframework.http.HttpMethod.POST;
-import static org.springframework.http.HttpMethod.PUT;
-
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -16,12 +11,13 @@ public class CorsFilter implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins(
-                    "http://localhost:3000",
-                    "http://localhost:5173",
-                    "http://127.0.0.1:5173",
-                    "http://192.168.0.170:5173",
-                    "http://192.168.0.188:5173",
-                    "http://118.131.63.237:5173"
+                    // "http://localhost:3000",
+                    // "http://localhost:5173",
+                    // "http://127.0.0.1:5173",
+                    // "http://192.168.0.170:5173",
+                    // "http://192.168.0.188:5173",
+                    // "http://118.131.63.237:5173"
+                    "http://43.201.51.101"
                 ) // 허용할 출처
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 // .allowedHeaders("*")
